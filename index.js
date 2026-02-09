b5956ce9b34b45674d7e4aa9f048b3060a12f368
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-09 23:01:09 +0600

    added data in db and load data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
@@ -29,6 +29,12 @@ async function run() {
 
         const database = client.db('usersdb')
         const userCollection = database.collection('users')
+
+        app.get('/users', async(req,res)=>{
+            const cursor = userCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
         app.post('/users', async (req, res) => {
             try {
